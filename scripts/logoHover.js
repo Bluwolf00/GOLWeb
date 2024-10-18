862861f6d3631a9cc333f5b125c35eeafe9e1109
Bluwolf00 <62963173+Bluwolf00@users.noreply.github.com>
2024-10-18 18:25:11 +0100

    Initial Commit

---
+++ scripts/logoHover.js
@@ -0,0 +1,12 @@
+
+function onLogoHover() {
+    logo = document.getElementById("front-logo");
+
+    logo.src = "img/logoAnim.gif";
+}
+
+function onLogoOut() {
+    logo = document.getElementById("front-logo");
+
+    logo.src = "img/logo.png";
+}
