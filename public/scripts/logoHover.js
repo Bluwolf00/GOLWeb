de928b65d501ce70f0b71b1656d8ce19c87399df
Bluwolf00 <62963173+Bluwolf00@users.noreply.github.com>
2024-10-23 18:24:47 +0100

    Convert to Express

---
+++ public/scripts/logoHover.js
@@ -0,0 +1,12 @@
+
+function onLogoHover() {
+    logo = document.getElementById("front-logo");
+
+    logo
+}
+
+function onLogoOut() {
+    logo = document.getElementById("front-logo");
+
+    logo.src = "img/logo.png";
+}
