970ada82ba3d8f839857e4a0be30a0bdc005cb3b
Bluwolf00 <62963173+Bluwolf00@users.noreply.github.com>
2025-03-08 17:15:12 +0000

    Update Org Chart Colouring

--- public/scripts/orgchart.js
+++ public/scripts/orgchart.js
@@ -30,11 +30,12 @@ function createOrg(data) {
         .nodeContent(function (d, i, arr, state) {
             const color = 'transparent';
             const imageDiffVert = 25 + 2;
-            const textcolor = "#2287ad";
+            const textcolor = "rgb(90, 137, 238)";
             if (d.data.rankName == "Reserve") {
+                const nodeColor = 'rgba(255, 234, 116, 0.2)';
                 return `
                             <div style='width:${d.width}px;height:${d.height}px;padding-top:${imageDiffVert - 2}px;padding-left:1px;padding-right:1px'>
-                                <div class="node-bubble" style="font-family: 'Inter', sans-serif;background-color:${color};  margin-left:-1px;width:${d.width - 2}px;height:${d.height - imageDiffVert}px;border-radius:10px;border: 1px solid #E4E2E9">
+                                <div class="node-bubble" style="font-family: 'Inter', sans-serif;background-color:${nodeColor};  margin-left:-1px;width:${d.width - 2}px;height:${d.height - imageDiffVert}px;border-radius:10px;border: 1px solid #E4E2E9">
                                     <div style="color:#FFFFFF;display:flex;justify-content:flex-end;margin-top:5px;margin-right:8px">#${d.data.nodeId}</div>
                                     <div style="display:flex;justify-content:flex-end;margin-top:5px">   <img src="img/badge/Placeholder_Badge.png" style="margin-right:${8}px;border-radius:0px;width:25px;height:25px;" /></div>
                                     <div style="background-color:${color};margin-top:${-imageDiffVert - 32}px;margin-left:${15}px;border-radius:100px;width:50px;height:50px;" ></div>
@@ -43,16 +44,35 @@ function createOrg(data) {
                     } </div>
                                         <div style="color:${textcolor};margin-left:20px;margin-top:3px;font-size:10px;"> ${d.data.rankName
                     } </div>
-                                        <div style="color:${textcolor};margin-left:20px;margin-top:3px;font-size:10px;"> ${d.data.Nick
+                                        <div style="color:'#FFFFFF';margin-left:20px;margin-top:3px;font-size:10px;"> ${d.data.Nick
                     } </div>
                 
                                     </div></a>
                                 </div>
                                         `;
             } else {
+                var nodeColor;
+                switch (d.data.rankName) {
+                    case "Sergeant":
+                    case "Corporal":
+                    case "Second Lieutenant":
+                    case "First Lieutenant":
+                        nodeColor = 'rgba(174, 0, 255, 0.2)';
+                        
+                        break;
+
+                    case "Airman":
+                    case "First Class Airman":
+                        nodeColor = 'rgba(0, 81, 255, 0.2)';
+                        break;
+
+                    default:
+                        nodeColor = 'rgba(255, 38, 0, 0.2)';
+                        break;
+                }
                 return `
                             <div style='width:${d.width}px;height:${d.height}px;padding-top:${imageDiffVert - 2}px;padding-left:1px;padding-right:1px'>
-                                <div class="node-bubble" onclick="window.location.href = 'https://gol-clan.com/profile?name=${d.data.UName}'" style="font-family: 'Inter', sans-serif;background-color:${color};  margin-left:-1px;width:${d.width - 2}px;height:${d.height - imageDiffVert}px;border-radius:10px;border: 1px solid #E4E2E9">
+                                <div class="node-bubble" onclick="window.location.href = 'https://gol-clan.com/profile?name=${d.data.UName}'" style="font-family: 'Inter', sans-serif;background-color:${nodeColor};  margin-left:-1px;width:${d.width - 2}px;height:${d.height - imageDiffVert}px;border-radius:10px;border: 1px solid #E4E2E9">
                                     <div style="color:#FFFFFF;display:flex;justify-content:flex-end;margin-top:5px;margin-right:8px">#${d.data.nodeId}</div>
                                     <div style="display:flex;justify-content:flex-end;margin-top:5px">   <img src=" ${d.data.rankPath}" style="margin-right:${8}px;border-radius:0px;width:25px;height:25px;" /></div>
                                     <div style="background-color:${color};margin-top:${-imageDiffVert - 32}px;margin-left:${15}px;border-radius:100px;width:50px;height:50px;" ></div>
@@ -61,7 +81,7 @@ function createOrg(data) {
                     } </a></div>
                                         <div style="color:${textcolor};margin-left:20px;margin-top:3px;font-size:10px;"> ${d.data.rankName
                     } </div>
-                                        <div style="color:${textcolor};margin-left:20px;margin-top:3px;font-size:10px;"> ${d.data.Nick
+                                        <div style="color:'#FFFFFF';margin-left:20px;margin-top:3px;font-size:10px;"> ${d.data.Nick
                     } </div>
                 
                                     </div></a>
