37332a0377fde46946673fd78bf22facf0228832
Bluwolf00 <62963173+Bluwolf00@users.noreply.github.com>
2025-05-01 16:18:42 +0100

    Remove Lance Corporal from Leadership

--- public/scripts/orgchart.js
+++ public/scripts/orgchart.js
@@ -54,7 +54,6 @@ function createOrg(data) {
                 var nodeColor;
                 switch (d.data.rankName) {
                     case "Sergeant":
-                    case "Lance Corporal":
                     case "Corporal":
                     case "Second Lieutenant":
                     case "First Lieutenant":
