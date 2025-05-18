402e52e16c591b959d14846e46bf563b8ead23b3
Bluwolf00 <62963173+Bluwolf00@users.noreply.github.com>
2025-05-18 23:26:46 +0100

    Include bycrypt in data

--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var router = express.Router();
 const db = require('../database.js');
+const bcrypt = require('bcrypt');
 
 const middle = require('../middle.js');
 const authPage = middle.authPage;
