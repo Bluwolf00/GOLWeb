7f810f61dc54a8239385614366f7b20b1c01f653
Bluwolf00 <62963173+Bluwolf00@users.noreply.github.com>
2025-07-15 16:56:24 +0100

    Mobile Compat

--- public/scripts/observer.js
+++ public/scripts/observer.js
@@ -1,13 +1,14 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
+      // console.log('Element is intersecting:', entry.target, entry.intersectionRatio, entry.boundingClientRect, entry.rootBounds);
       entry.target.classList.add('show');
     //   observer.unobserve(entry.target);
     } else {
         entry.target.classList.remove('show');
     }
   });
-}, { threshold: 0.1 });
+}, { threshold: 0.1});
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => {
